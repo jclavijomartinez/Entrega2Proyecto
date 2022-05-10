cbecdb4c531248da7262cdb6164aea5afda8a4df
Juan Clavijo <jclavijomartinez@gmail.com>
2022-05-09 19:59:45 -0500

    cambios a pantalla galería

--- src/PantallaGaleria.java
+++ src/PantallaGaleria.java
@@ -1,4 +1,10 @@
 import java.util.*;
 public class PantallaGaleria {
-    
+    public static void main(String[] args) {
+        try (Scanner entrada = new Scanner(System.in)) {
+            entrada.nextInt();
+        } catch (NoSuchElementException e) {
+            /
+        }
+    }
 }
