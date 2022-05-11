3377033673d3e44cc4fddaf18260422b6b27072c
Juan Clavijo <jclavijomartinez@gmail.com>
2022-05-11 18:21:15 -0500

    gestionobras listo y controlgalería falta leer el documento

--- src/ControlGaleria.java
+++ src/ControlGaleria.java
@@ -1,3 +1,34 @@
-public class ControlGaleria {
+import java.io.*;
+import java.util.*;
 
+public class ControlGaleria {
+    //crear la coleccion de obras, se llama listaObras
+    private static ArrayList<Obra> listaObras; 
+    //crear la lista de artistas, se llama lista artistas
+    private static ArrayList<Artista> listaArtistas;
+    // dato de tipo gestionobras para acceder a la creacion de artistas y obras
+    GestionObras gestionobras = new GestionObras();
+    // leer los datos del txt y agregarlos a la obra
+    public void leertxtyllenarobras(String ruta) {
+        File entradatxt = new File(ruta);
+        try (FileReader fr = new FileReader(ruta)) {
+            try (Scanner lectura = new Scanner(entradatxt)) {
+                BufferedReader bf =new BufferedReader(fr);
+                Long cantlin = bf.lines().count();
+                String linea = lectura.nextLine(); //linea de encabezado
+                for (int i = 1; i < cantlin; i++) {
+                    String lineai = lectura.nextLine();
+                    String[] contenidoObras = lineai.split("-");
+                    
+                }
+            } catch (FileNotFoundException e) { //excepcion del scanner
+                System.out.println("el archivo no existe revisa");
+            }
+        } catch (IOException e) { //excepcion del filereader
+            System.out.println("hay un problema con el archivo");
+        }
+    }
+    public void agregarObra(Obra nuevaobra) {
+        listaObras.add(nuevaobra);
+    }
 }
--- src/GestionObras.java
+++ src/GestionObras.java
@@ -1,3 +1,12 @@
 public class GestionObras {
-    
+    // crear obra
+    static public Obra crearObra(long codigoObra, String titulo, int dia, int mes, int anio,float precioRef,String dimensiones) {
+        Obra obraAAgregar = new Obra(codigoObra, titulo, dia, mes, anio, precioRef, dimensiones);
+        return obraAAgregar;
+    }
+    // crear artista
+    static public Artista crearArtista( long codigoArtista, long cedula, String nombre, String apellido, int dia, int mes, int anio, long telefono) {
+        Artista artistaAagregar = new Artista(codigoArtista, cedula, nombre, apellido, dia, mes, anio, telefono);
+        return artistaAagregar;
+    }
 }
