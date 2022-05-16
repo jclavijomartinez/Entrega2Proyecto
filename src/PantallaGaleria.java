4b44a74c0522ca112076d507a393fb798b94be97
Juan David Sanchez <juandsanchez@javeriana.edu.co>
2022-05-16 15:13:07 -0500

    case 2 y 3

--- src/PantallaGaleria.java
+++ src/PantallaGaleria.java
@@ -6,10 +6,16 @@ public class PantallaGaleria {
             System.out.println("¡TE DAMOS LA BIENVENIDA A LA GALERÍA! estas son las opciones que puedes elegir");
             System.out.println("1. leer datos desde archivo");
             System.out.println("2. ver listado de obras disponibles");
+            System.out.println("3. Agregar una obra");
             System.out.print("¿Que quieres hacer?: ");
             opcion=entrada.nextInt();
             switch (opcion) {
                 case 1:
+                    break;
+                case 2:
+                    break;
+
+                case 3: 
                     break;
                 default:
                     System.out.println("no reconozco esa opcion");
